661a179cec3fbac20686665d3c8a256845499fe1
Jens Page <jens.page@datajockey.org>
2014-05-28 18:38:12 -0400

    formatting

--- src/main/java/com/playgrid/bukkit/plugin/command/RegisterCommandExecutor.java
+++ src/main/java/com/playgrid/bukkit/plugin/command/RegisterCommandExecutor.java
@@ -15,16 +15,14 @@ import com.playgrid.api.client.manager.PlayerManager;
 import com.playgrid.api.entity.PlayerRegistration;
 import com.playgrid.bukkit.plugin.PlayGridMC;
 
+
 public class RegisterCommandExecutor implements CommandExecutor {
 
 	private PlayGridMC plugin;
 	
-	
 	public RegisterCommandExecutor(PlayGridMC plugin) {
 		this.plugin = plugin;
 	}
-
-	
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -47,13 +45,9 @@ public class RegisterCommandExecutor implements CommandExecutor {
 			bPlayer.sendMessage("An error occurred and your request could not be completed at this time.");
 			return false;
 		}
-		
-		
 	}
 	
-
 	private boolean register(Player bPlayer, String email) {
-
 		PlayerManager playerManager = RestAPI.getInstance().getPlayerManager();
 		String player_uid = bPlayer.getUniqueId().toString().replaceAll("-", "");
 		
@@ -68,7 +62,6 @@ public class RegisterCommandExecutor implements CommandExecutor {
 		}
 		
 		if (playerRegistration.message.equals("SUCCESS")) {
-
 			String[] messages = new String[] {
 					"You have successfully registered.", 
 					String.format("Check your %s account for instructions to finalize your registration.", playerRegistration.email),
@@ -83,13 +76,12 @@ public class RegisterCommandExecutor implements CommandExecutor {
 
 			plugin.permissions.setGroups(bPlayer, pPlayer.permission_groups);
 			plugin.getLogger().info(pPlayer.name + " registered and was added to the " + Arrays.toString(pPlayer.permission_groups) + " groups.");
-
 			
 		} else if (playerRegistration.message.equals("REJECTED")) {
 			String[] messages = new String[] {
 					String.format("%s is already in use.", playerRegistration.email), 
 					String.format("Visit %s to manage your players.", plugin.game.website),
-					};
+			};
 			bPlayer.sendMessage(messages);
 
 		} else if (playerRegistration.message.equals("ALREADY REGISTERED")) {
@@ -98,10 +90,8 @@ public class RegisterCommandExecutor implements CommandExecutor {
 		} else {
 			plugin.getLogger().info("Unrecognized registration message: " + playerRegistration.message);
 			return false;
-		
 		}
 
 		return true;
 	}
-
 }
